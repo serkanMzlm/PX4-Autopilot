8bb94a2d5a0516a38e715060807e0cc4700718a0
serkan <serkanmazlum306@gmail.com>
2025-03-20 13:01:04 +0300

    gz yükselitldi

--- src/drivers/distance_sensor/tf02pro/TF02PRO.hpp
+++ src/drivers/distance_sensor/tf02pro/TF02PRO.hpp
@@ -50,8 +50,8 @@
 #define TF02PRO_BASEADDR			0x10 	// 7-bit address. 8-bit address is 0x20.
 
 /* Device limits */
-#define TF02PRO_MIN_DISTANCE 			(0.10f)
-#define TF02PRO_MAX_DISTANCE 			(35.00f)
+#define TF02PRO_MIN_DISTANCE 			(0.05f)
+#define TF02PRO_MAX_DISTANCE 			(7.00f)
 
 #define TF02PRO_CONVERSION_INTERVAL 		100000	// 100 ms for one sensor.
 
